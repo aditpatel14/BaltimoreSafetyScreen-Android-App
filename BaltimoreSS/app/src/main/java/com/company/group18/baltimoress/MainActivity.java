25a6d37ce69a742855db7d6a729759a6bdb25dd5
CURTIS\Curtis <curtisjones@rogers.com>
2017-03-16 09:06:54 -0400

    testing

--- BaltimoreSS/app/src/main/java/com/company/group18/baltimoress/MainActivity.java
+++ BaltimoreSS/app/src/main/java/com/company/group18/baltimoress/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
+//hey
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
